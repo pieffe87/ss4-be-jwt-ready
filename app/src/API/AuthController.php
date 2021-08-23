9c729a7503e275bc00f9900730d53c4022bf46aa
Pier Francesco Ferrari <pier.ferrari@gmail.com>
2021-08-23 18:07:40 +0200

    AuthCtrl - Cleaned code with jsonStandardMessage + getCurrentToken + getCurrentMember + getJsonData

--- app/src/API/AuthController.php
+++ app/src/API/AuthController.php
@@ -22,77 +22,84 @@ namespace {
 
     public function login()
     {
-      $json = file_get_contents('php://input');
-      $data = json_decode($json);
-      if(isset($data->email) && $data->email != '' && isset($data->password) && $data->password != '') {
-        $this->getRequest()->addHeader('PHP_AUTH_USER', $data->email);
-        $this->getRequest()->addHeader('PHP_AUTH_PW', $data->password);
+      $data = $this->getJsonData();
+      if (!is_array($data)) return $data;
+
+      if(!isset($data['email']) || $data['email'] == '') {
+        $data = [
+          'errors' => [
+            [
+              'field' => 'email',
+              'msg' => 'Campo email non valido'
+            ]
+          ]
+        ];
+        return $this->jsonResponse($data, 422);
+      }
+      if(!isset($data['password']) || $data['password'] == '') {
+        $data = [
+          'errors' => [
+            [
+              'field' => 'password',
+              'msg' => 'Campo password non valido'
+            ]
+          ]
+        ];
+        return $this->jsonResponse($data, 422);
+      }
+      if(isset($data['email']) && $data['email'] != '' && isset($data['password']) && $data['password'] != '') {
+        $this->getRequest()->addHeader('PHP_AUTH_USER', $data['email']);
+        $this->getRequest()->addHeader('PHP_AUTH_PW', $data['password']);
 
         try {
           $payload = JWTUtils::inst()->byBasicAuth($this->getRequest());
 
           if(is_array($payload) && array_key_exists('token', $payload)) {
             $data = [
-              'code' => 'OK',
-              'access_token' => $payload['token'],
-              'token_type' => 'bearer',
-              'data' => [
-                'user' => [
-                  'email' => $payload['member']['email'],
-                  'firstname' => $payload['member']['firstName'],
-                  'surname' => $payload['member']['surname']
-                ]
-              ],
-              'meta' => [
-                'message' => 'Accesso effettuato correttamente',
-              ]
+              'status' => 'OK'
             ];
             return $this->jsonResponse($data, 200, $payload['token']);
           }
         }
         catch(JWTUtilsException $e) {
-          return $this->jsonResponse($this->jsonStandardErrorData($e->getMessage()), 200);
+          return $this->jsonResponse($this->jsonStandardMessage('KO', $e->getMessage()), 200);
         }
       }
-      return $this->jsonResponse($this->jsonStandardErrorData('Dati mancanti'), 400);
     }
 
     public function refresh()
     {
-      if ($this->getRequest()->getHeader('Authorization') !== null) {
-        $header = $this->getRequest()->getHeader('Authorization');
-        list($bearer, $firstToken) = explode(' ',$header);
-        try {
-          $renewedToken = JWTUtils::inst()->renew($firstToken);
-        } catch(JWTUtilsException $e) {
-          return $this->jsonResponse(['msg' => 'Unauthorized'], 401);
-        }
-        if($renewedToken != $firstToken) {
-          return $this->jsonResponse($this->jsonStandardSuccessData('Renewed'), 200, $renewedToken);
-        }
-        return $this->jsonResponse($this->jsonStandardSuccessData('Nothing to do'), 200);
+      $firstToken = $this->getCurrentToken();
+      if (!is_string($firstToken)) return $firstToken;
+
+      try {
+        $renewedToken = JWTUtils::inst()->renew($firstToken);
+      } catch(JWTUtilsException $e) {
+        return $this->jsonResponse($this->jsonStandardMessage('KO', 'Unauthorized.'), 401);
       }
-      return $this->jsonResponse($this->jsonStandardErrorData('Dati mancanti'), 400);
+      if($renewedToken != $firstToken) {
+        return $this->jsonResponse($this->jsonStandardMessage('OK', 'Auth token renewed.'), 200, $renewedToken);
+      }
+      return $this->jsonResponse($this->jsonStandardMessage('OK', 'Auth token still valid. Nothing to do.'), 200);
     }
 
     public function user()
     {
       $member = $this->getCurrentMember();
+      if (!isset($member->ID)) return $member;
+
       if ($member) {
         $data = [
-          'code' => 'OK',
+          'status' => 'OK',
           'data' => [
-            'user' => [
-              'email' => $member->Email,
-              'firstname' => $member->FirstName,
-              'surname' => $member->Surname
-            ]
+            'email' => $member->Email,
+            'firstname' => $member->FirstName,
+            'surname' => $member->Surname,
+            'type' => 'admin'
           ]
         ];
         return $this->jsonResponse($data, 200);
       }
-
-      return $this->jsonResponse($this->jsonStandardErrorData('Dati mancanti'), 400);
     }
 
   }
